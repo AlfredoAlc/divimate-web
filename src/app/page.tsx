ec2834c88ea1b2a925b944b7bfb1bc328b274107
Alfredo Alcántara <alfredo.alcantara.r@gmail.com>
2025-09-18 10:47:44 -0700

    preload links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import "./globals.css";
 
+import Head from "next/head";
+
 import Info1 from "@/components/Animations/Info1";
 import Info2 from "@/components/Animations/Info2";
 import Info3 from "@/components/Animations/Info3";
@@ -13,13 +15,40 @@ import Hero from "@/components/Hero";
 import Outro from "@/components/Outro";
 import { VideoCarrouselProvider } from "@/contexts/VideoCarrouselProvider";
 import useTheme from "@/hooks/useTheme";
-import { DARK_VIDEOS, LIGHT_VIDEOS, Sections } from "@/utils/InfoUtils";
+import {
+  DARK_GROUP_VIDEOS,
+  DARK_VIDEOS,
+  LIGHT_GROUP_VIDEOS,
+  LIGHT_VIDEOS,
+  Sections,
+} from "@/utils/InfoUtils";
 
 export default function Home() {
   const theme = useTheme();
 
   return (
     <div className="page">
+      <Head>
+        {theme === "dark"
+          ? [...DARK_VIDEOS, ...DARK_GROUP_VIDEOS].map((v) => (
+              <link
+                key={v}
+                rel="preload"
+                as="video"
+                href={v}
+                type="video/mov"
+              />
+            ))
+          : [...LIGHT_VIDEOS, ...LIGHT_GROUP_VIDEOS].map((v) => (
+              <link
+                key={v}
+                rel="preload"
+                as="video"
+                href={v}
+                type="video/mov"
+              />
+            ))}
+      </Head>
       <VideoCarrouselProvider>
         <main>
           <Hero />
