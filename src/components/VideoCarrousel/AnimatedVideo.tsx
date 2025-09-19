5bfdcbd475215d18f715724048bb563a249d76fe
Alfredo Alcántara <alfredo.alcantara.r@gmail.com>
2025-09-19 12:49:51 -0700

    let's see

--- src/components/VideoCarrousel/AnimatedVideo.tsx
+++ src/components/VideoCarrousel/AnimatedVideo.tsx
@@ -57,7 +57,7 @@ export default forwardRef<HTMLVideoElement, VideoCarrouselProps>(
               loop
               muted
               playsInline
-              autoPlay={false}
+              //autoPlay={false}
               className={styles.videoStyle}
               poster={poster}
             />
